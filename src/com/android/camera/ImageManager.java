068ceec7a8f0dab6f1abb1894cad30e3e437bed2
Motorhead1991 <motor-head-1991@hotmail.com>
2013-09-10 10:38:10 -0700

    Another class update

--- src/com/android/camera/ImageManager.java
+++ src/com/android/camera/ImageManager.java
@@ -25,6 +25,7 @@ import com.android.camera.gallery.VideoList;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
