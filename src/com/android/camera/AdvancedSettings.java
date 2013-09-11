5ebeb884c069e07b0052351a3fdbdb600bc729f8
Motorhead1991 <motor-head-1991@hotmail.com>
2013-09-10 21:55:31 -0700

    Reverted onPreferenceChangeListener import

--- src/com/android/camera/AdvancedSettings.java
+++ src/com/android/camera/AdvancedSettings.java
@@ -31,7 +31,6 @@ import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
-import android.preference.OnPreferenceChangeListener;
 
 public class AdvancedSettings extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
 
